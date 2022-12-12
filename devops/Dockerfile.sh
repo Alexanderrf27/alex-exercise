b584874426577bb83a2725f161dbb054259aed55
arrodriguez <arrodriguez@paradigmadigital.com>
2022-12-13 00:16:40 +0100

    12122022 - Inditex practice exercises

--- devops/Dockerfile.sh
+++ devops/Dockerfile.sh
@@ -3,5 +3,3 @@ COPY ./build/libs/alex-exercise-0.0.1-SNAPSHOT.jar /usr/app/
 WORKDIR /usr/app
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "alex-exercise-0.0.1-SNAPSHOT.jar"]
-
-alex-exercise-0.0.1-SNAPSHOT.jar
